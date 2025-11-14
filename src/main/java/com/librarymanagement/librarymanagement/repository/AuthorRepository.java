9760a53df354816103e0bef1c8a331aaf550a48e
amiraghelan <amirhaghelan@gmail.com>
2025-11-14 20:50:36 +0330

    feat(repository) add Author and Book repository

---
+++ src/main/java/com/librarymanagement/librarymanagement/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.librarymanagement.librarymanagement.repository;
+
+import com.librarymanagement.librarymanagement.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+}
